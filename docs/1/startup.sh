74fec4d072d0e977d39c5b2b26434dc367aeaf61
ytyaru <yttry0@gmail.com>
2021-01-26 17:42:11 +0900

    up

--- docs/1/startup.sh
+++ docs/1/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm init -y
-npm i -D webpack webpack-cli webpack-dev-server
+npm i -D webpack webpack-cli webpack-dev-server babel-loader @babel/core @babel/preset-env @babel/plugin-proposal-private-methods
 npx webpack
 npx webpack serve
